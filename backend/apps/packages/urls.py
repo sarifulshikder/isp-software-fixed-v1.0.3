cd0e6d76d387adc054f46147b359713a4bcc5ef4
sarifulshikder <90824320+sarifulshikder@users.noreply.github.com>
2026-04-28 19:23:19 +0600

    Create urls.py

---
+++ backend/apps/packages/urls.py
@@ -0,0 +1,5 @@
+from rest_framework.routers import DefaultRouter
+from .views import PackageViewSet
+router = DefaultRouter()
+router.register('', PackageViewSet, basename='package')
+urlpatterns = router.urls
