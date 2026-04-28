d799caff8efe037f5988dded1319e33c5cc7124a
sarifulshikder <90824320+sarifulshikder@users.noreply.github.com>
2026-04-28 19:23:46 +0600

    Create urls.py

---
+++ backend/apps/payments/urls.py
@@ -0,0 +1,5 @@
+from rest_framework.routers import DefaultRouter
+from .views import PaymentViewSet
+router = DefaultRouter()
+router.register('', PaymentViewSet, basename='payment')
+urlpatterns = router.urls
